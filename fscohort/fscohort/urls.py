259a7f854bb0622923652efc5917dc52a291c4bf
bemrekalkan <bemre.kalkan@gmail.com>
2022-08-27 11:29:10 +0300

    ...

--- fscohort/fscohort/urls.py
+++ fscohort/fscohort/urls.py
@@ -1,21 +1,11 @@
-"""fscohort URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
+from .views import home,student_list, student_add, student_detail, student_update,student_delete
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', home, name="home"),
+    path('student_list/', student_list, name="list"),
+    path('student_add/', student_add, name="add"),
+    path('detail/<int:id>/', student_detail, name="detail"),
+    path('update/<int:id>/', student_update, name="update"),
+    path('delete/<int:id>/', student_delete, name="delete"),
 ]
